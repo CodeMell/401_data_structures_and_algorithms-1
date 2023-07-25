69aa1b07ff59b7b8da887e9a9709f17fcb173f8a
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-07-24 20:31:35 -0500

    Update LinkedListKth.cs

--- c-sharp/CodeChallenge7/LinkedListKth.cs
+++ c-sharp/CodeChallenge7/LinkedListKth.cs
@@ -1,10 +1,34 @@
 namespace CodeChallenges 
 {
-    public class LinkedListKth: InsertableLinkedList
+  public class LinkedListKth : LinkedList
+  {
+    public int KthFromEnd(int kthFromEnd)
     {
-        public void KthFromEnd(int kthFromEnd)
-        {
-            
-        }
+      if (kthFromEnd < 0)
+        throw new ArgumentException("kthFromEnd must be a non-negative integer.");
+
+      Node slow = Head;
+      Node fast = Head;
+
+      // Move the fast pointer k positions ahead
+      for (int i = 0; i < kthFromEnd; i++)
+      {
+        if (fast == null)
+          throw new ArgumentException("kthFromEnd is larger than the linked list size.");
+        fast = fast.Next;
+      }
+
+      if (fast == null)
+        throw new ArgumentException("kthFromEnd is larger than the linked list size.");
+
+      // Move both pointers until the fast pointer reaches the end
+      while (fast.Next != null)
+      {
+        slow = slow.Next;
+        fast = fast.Next;
+      }
+
+      return slow.Value -10;
     }
+  }
 }
