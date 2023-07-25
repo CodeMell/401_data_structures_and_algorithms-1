acb084197a3dd7c03982c4eff5e5fd57bd517062
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-07-24 20:50:15 -0500

    Update LinkedListZip.cs

--- c-sharp/CodeChallenge8/LinkedListZip.cs
+++ c-sharp/CodeChallenge8/LinkedListZip.cs
@@ -6,9 +6,26 @@ namespace CodeChallenges
     public static LinkedListKth ZipLists(LinkedListKth list1, LinkedListKth list2)
     {
       LinkedListKth result = new LinkedListKth();
+      Node current1 = list1.Head;
+      Node current2 = list2.Head;
 
+      while (current1 != null || current2 != null)
+      {
+        if (current1 != null)
+        {
+          result.Insert(current1.Value);
+          current1 = current1.Next;
+        }
+
+        if (current2 != null)
+        {
+          result.Insert(current2.Value);
+          current2 = current2.Next;
+        }
+      }
 
       return result;
     }
+
   }
 }
