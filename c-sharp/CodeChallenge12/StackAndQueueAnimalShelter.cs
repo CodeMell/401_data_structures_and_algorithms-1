2e1861e4c66e287972b4067c7818de49d45ddbf4
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-08-01 09:57:01 -0500

    Update StackAndQueueAnimalShelter.cs

--- c-sharp/CodeChallenge12/StackAndQueueAnimalShelter.cs
+++ c-sharp/CodeChallenge12/StackAndQueueAnimalShelter.cs
@@ -1,22 +1,53 @@
-namespace CodeChallenges 
+using System;
+using System.Collections.Generic;
+
+namespace CodeChallenges
 {
+  public class Animal
+  {
+    public string Species { get; set; }
+    public string Name { get; set; }
 
-    public class Animal
+    public Animal(string species, string name)
     {
-        
+      Species = species;
+      Name = name;
+    }
+  }
+
+  public class AnimalShelter
+  {
+    private Queue<Animal> animalsQueue;
+
+    public AnimalShelter()
+    {
+      animalsQueue = new Queue<Animal>();
     }
 
-
-    public class StackAndQueueAnimalShelter
+    public void Enqueue(Animal animal)
     {
-        public void Enqueue(Animal animal)
+      animalsQueue.Enqueue(animal);
+    }
+
+    public Animal Dequeue(string pref)
+    {
+      if (pref != "dog" && pref != "cat")
+      {
+        return null;
+      }
+
+      // Find the first animal with the preferred species in the queue
+      foreach (var animal in animalsQueue)
+      {
+        if (animal.Species == pref)
         {
-            
+          animalsQueue = new Queue<Animal>(animalsQueue); // To keep the queue intact while removing the animal
+          animalsQueue.Dequeue(); // Remove the animal from the front of the queue
+          return animal; // Return the animal with the preferred species
         }
+      }
 
-        // public Animal Dequeue(string pref)
-        // {
-            
-        // }
+      return null; // Return null if no animal with the preferred species is found
     }
+  }
 }
