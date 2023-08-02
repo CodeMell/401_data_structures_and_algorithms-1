819bd39671d866f154d523e66ef03de75b0c7680
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-08-02 09:27:59 -0500

    Update MaxStack.cs

--- c-sharp/CodeChallenge14/MaxStack.cs
+++ c-sharp/CodeChallenge14/MaxStack.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace CodeChallenges
 {
@@ -6,14 +7,55 @@ namespace CodeChallenges
   {
     public class MaxStack
     {
+      private Stack<int> stack;
+      private Stack<int> maxStack;
 
+      public MaxStack()
+      {
+        stack = new Stack<int>();
+        maxStack = new Stack<int>();
+      }
+
+      public void Push(int value)
+      {
+        stack.Push(value);
+
+        if (maxStack.Count == 0 || value >= maxStack.Peek())
+        {
+          maxStack.Push(value);
+        }
+      }
+
+      public int Pop()
+      {
+        if (stack.Count == 0)
+        {
+          throw new InvalidOperationException("Stack is empty.");
+        }
+
+        int poppedValue = stack.Pop();
+        if (poppedValue == maxStack.Peek())
+        {
+          maxStack.Pop();
+        }
+
+        return poppedValue;
+      }
+
+      public int GetMax()
+      {
+        if (maxStack.Count == 0)
+        {
+          throw new InvalidOperationException("Stack is empty.");
+        }
+
+        return maxStack.Peek();
+      }
     }
 
     public string DuckDuckGoose(string[] stringArray, int k)
     {
-        return "";
+      return "";
     }
   }
-
-
 }
